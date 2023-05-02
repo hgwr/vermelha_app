73bb6abcf7805df99595683e273bd2bc22a0b4de
Shigeru Hagiwara <hgwrsgr@gmail.com>
2023-05-03 06:11:24 +0900

    character screen

--- lib/screens/character_screen.dart
+++ lib/screens/character_screen.dart
@@ -31,78 +31,98 @@ class _CharacterScreenState extends State<CharacterScreen> {
               padding: const EdgeInsets.all(16.0),
               child: Form(
                 key: _formKey,
-                child: ListView(
-                  shrinkWrap: true,
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextField(
-                            decoration:
-                                const InputDecoration(labelText: 'Name'),
-                            controller: TextEditingController(
-                              text: character.name,
-                            ),
-                            onChanged: (value) {
-                              character.name = value;
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 60,
-                      child: Row(
-                        children: [
-                          Text('Job: '),
-                          Expanded(
-                            child: DropdownButton<Job>(
-                              value: character.job,
-                              onChanged: (Job? newValue) {
-                                setState(() {
-                                  character.job = newValue!;
-                                });
-                              },
-                              items: Job.values.map((Job job) {
-                                return DropdownMenuItem<Job>(
-                                  value: job,
-                                  child: Text(job.name),
-                                );
-                              }).toList(),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 60,
-                      child: Row(
-                        children: [
-                          Text('Level: '),
-                          Expanded(
-                            child: Text(
-                              character.level.toString(),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 60,
-                      child: Row(
-                        children: [
-                          Text('Max HP: '),
-                          Expanded(
-                            child: Text(
-                              character.maxHp.toString(),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                child: createListView(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  ListView createListView() {
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: SizedBox(
+                height: 100,
+                child: getImageByJob(character.job!),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                controller: TextEditingController(
+                  text: character.name,
+                ),
+                onChanged: (value) {
+                  character.name = value;
+                },
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 60,
+          child: Row(
+            children: [
+              Text('Job: '),
+              Expanded(
+                child: DropdownButton<Job>(
+                  value: character.job,
+                  onChanged: (Job? newValue) {
+                    setState(() {
+                      character.job = newValue!;
+                      character = getInitializedCharacterByJob(character.job!);
+                    });
+                  },
+                  items: Job.values.map((Job job) {
+                    return DropdownMenuItem<Job>(
+                      value: job,
+                      child: Text(job.name),
+                    );
+                  }).toList(),
                 ),
               ),
+            ],
+          ),
+        ),
+        characterPropertyItem(
+          'Level: ',
+          character.level.toString(),
+        ),
+        characterPropertyItem(
+          'HP: ',
+          "${character.hp} / ${character.maxHp}",
+        ),
+        characterPropertyItem(
+          'MP: ',
+          "${character.mp} / ${character.maxMp}",
+        ),
+        characterPropertyItem('Attack: ', character.attack.toString()),
+        characterPropertyItem('Defense: ', character.defense.toString()),
+        characterPropertyItem('Magic Power: ', character.magicPower.toString()),
+        characterPropertyItem('Speed: ', character.speed.toString()),
+      ],
+    );
+  }
+
+  SizedBox characterPropertyItem(String label, String value) {
+    return SizedBox(
+      height: 40,
+      child: Row(
+        children: [
+          Text(label),
+          Expanded(
+            child: Text(
+              value,
             ),
           ),
         ],
