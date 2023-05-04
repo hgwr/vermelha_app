e9d334bf8cb15ded4099fd16d2a778c0f6c6412d
Shigeru Hagiwara <hgwrsgr@gmail.com>
2023-05-04 09:41:28 +0900

    battle rules screen

--- lib/screens/edit_battle_rules_screen.dart
+++ lib/screens/edit_battle_rules_screen.dart
@@ -23,6 +23,7 @@ class EditBattleRulesScreen extends StatefulWidget {
 class _EditBattleRulesScreenState extends State<EditBattleRulesScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey();
   Character character = getInitializedCharacterByJob(Job.fighter);
+  bool _isOrdering = false;
 
   void saveCharacter() async {
     if (_formKey.currentState!.validate()) {
@@ -33,6 +34,33 @@ class _EditBattleRulesScreenState extends State<EditBattleRulesScreen> {
                 .updateCharacter(character);
       }
     }
+  }
+
+  void _showConfirmDeleteRuleDialog(BattleRule rule) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text("削除の確認"),
+        content: const Text("この戦闘ルールを削除しますか？"),
+        actions: [
+          TextButton(
+            child: const Text("いいえ"),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          ),
+          TextButton(
+            child: const Text("はい"),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+              setState(() {
+                character.battleRules.remove(rule);
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 
   BattleRule createNewBattleRule() {
@@ -59,6 +87,18 @@ class _EditBattleRulesScreenState extends State<EditBattleRulesScreen> {
       appBar: character.id != null
           ? AppBar(
               title: Text("戦闘ルール：${character.name}"),
+              actions: [
+                IconButton(
+                  icon: _isOrdering
+                      ? Icon(Icons.swap_vert_circle)
+                      : Icon(Icons.swap_vert),
+                  onPressed: () {
+                    setState(() {
+                      _isOrdering = !_isOrdering;
+                    });
+                  },
+                )
+              ],
             )
           : null,
       body: Column(
@@ -68,7 +108,9 @@ class _EditBattleRulesScreenState extends State<EditBattleRulesScreen> {
               padding: const EdgeInsets.all(16.0),
               child: Form(
                 key: _formKey,
-                child: createListView(),
+                child: _isOrdering
+                    ? createReorderableListView()
+                    : createListView(),
               ),
             ),
           ),
@@ -82,26 +124,48 @@ class _EditBattleRulesScreenState extends State<EditBattleRulesScreen> {
       children: [
         ...character.battleRules.map((battleRule) {
           return Card(
+            key: ValueKey(battleRule.priority),
             child: ListTile(
               leading: Text(battleRule.priority.toString()),
-              title: Text(battleRule.condition.name),
-              subtitle: Text(battleRule.action.name),
+              title: GestureDetector(
+                onTap: () {},
+                child: Row(
+                  children: [
+                    Text(battleRule.condition.name),
+                    const Icon(
+                      Icons.arrow_forward_ios,
+                      size: 18,
+                    )
+                  ],
+                ),
+              ),
+              subtitle: GestureDetector(
+                onTap: () {},
+                child: Row(
+                  children: [
+                    Text(battleRule.action.name),
+                    const Icon(
+                      Icons.arrow_forward_ios,
+                      size: 14,
+                    )
+                  ],
+                ),
+              ),
               trailing: IconButton(
-                icon: Icon(Icons.delete),
+                icon: const Icon(Icons.delete),
                 onPressed: () {
-                  setState(() {
-                    character.battleRules.remove(battleRule);
-                  });
+                  _showConfirmDeleteRuleDialog(battleRule);
                 },
               ),
             ),
           );
         }).toList(),
         Card(
+          key: const ValueKey("f4ed9d86-2c2b-4b52-b620-5c8f390b0f41"),
           child: ListTile(
-            title: Text("新しい戦闘ルールを追加する"),
+            title: const Text("新しい戦闘ルールを追加する"),
             trailing: IconButton(
-              icon: Icon(Icons.add),
+              icon: const Icon(Icons.add),
               onPressed: () {
                 setState(() {
                   character.battleRules.add(createNewBattleRule());
@@ -110,6 +174,47 @@ class _EditBattleRulesScreenState extends State<EditBattleRulesScreen> {
             ),
           ),
         ),
+      ],
+    );
+  }
+
+  ReorderableListView createReorderableListView() {
+    return ReorderableListView(
+      onReorder: (int start, int current) {
+        if (start < current) {
+          int end = current - 1;
+          BattleRule startItem = character.battleRules[start];
+          int i = 0;
+          int local = start;
+          do {
+            character.battleRules[local] = character.battleRules[++local];
+            i++;
+          } while (i < end - start);
+          character.battleRules[end] = startItem;
+        } else if (start > current) {
+          BattleRule startItem = character.battleRules[start];
+          for (int i = start; i > current; i--) {
+            character.battleRules[i] = character.battleRules[i - 1];
+          }
+          character.battleRules[current] = startItem;
+        }
+        for (int i = 1; i <= character.battleRules.length; i++) {
+          character.battleRules[i - 1] = character.battleRules[i - 1]
+              .copyWith(priority: character.battleRules.length - i + 1);
+        }
+        setState(() {});
+      },
+      children: [
+        ...character.battleRules.map((battleRule) {
+          return Card(
+            key: ValueKey(battleRule.priority),
+            child: ListTile(
+                leading: Text(battleRule.priority.toString()),
+                title: Text(battleRule.condition.name),
+                subtitle: Text(battleRule.action.name),
+                trailing: const Icon(Icons.drag_handle)),
+          );
+        }).toList(),
       ],
     );
   }
