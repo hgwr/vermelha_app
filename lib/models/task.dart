c490bf7f367e758673219624bbaec3de163a8a63
Shigeru Hagiwara <hgwrsgr@gmail.com>
2026-01-01 09:49:30 +0900

    update

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-import 'package:vermelha_app/db/db_migration.dart';
 import 'package:vermelha_app/models/action.dart';
 import 'package:vermelha_app/models/character.dart';
 import 'package:vermelha_app/models/vermelha_context.dart';
